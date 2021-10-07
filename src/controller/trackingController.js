453625e6cb41284f6c4667ebea85dece49ce66f3
Juan Freitas <juanvitor2001@outlook.com>
2021-10-07 14:16:43 -0300

    fix: unity in location field

--- src/controller/trackingController.js
+++ src/controller/trackingController.js
@@ -3,49 +3,50 @@ const { DateTime } = require('luxon');
 const Bottleneck = require('bottleneck');
 
 
-    
-const limiter = new Bottleneck({
-    maxConcurrent: 1,
-    minTime: 1500,
-    highWater: 10,
-    strategy:Bottleneck.strategy.OVERFLOW
-});
-    
-module.exports = {
-    async fetchTracking(req, res) {
-        
-        const counts = limiter.counts();
-        console.log(counts);
 
-        try {
-            const {trackingCode} = req.body;
-						if (!trackingCode){
-							console.log("Tracking code is null or invalid");
-							return res.status(400).send();
-						}
-            const { data } = await limiter.schedule({expiration: 1500},() => api.get('/', { params: { codigo: trackingCode } }));
-            const { codigo: code, ultimo: updatedAt, servico: type, eventos: events } = data;
-            return res.json({
-                code,
-                updatedAt,
-                type,
-                events: events.map(({ data: date, hora: time, local: location, status, subStatus }) => {
-                    const [city, state] = location.split('/');
-                    return {
-                        date: DateTime.fromFormat( `${date} ${time}`,'dd/MM/yyyy HH:mm' ).toISO(),
-                        city,
-                        state,
-                        status,
-                        subStatus
-                    }
-                })
-        });
-        } catch (err) {
-            if (err instanceof Bottleneck.BottleneckError) {
-                return res.status(503).send();
-            }
-            console.error(err)
-            return res.status(500).json({ status: 500 });
-        }
-    }
+const limiter = new Bottleneck({
+	maxConcurrent: 1,
+	minTime: 1500,
+	highWater: 10,
+	strategy: Bottleneck.strategy.OVERFLOW
+});
+
+module.exports = {
+	async fetchTracking(req, res) {
+
+		const counts = limiter.counts();
+		console.log(counts);
+
+		try {
+			const { trackingCode } = req.body;
+			if (!trackingCode) {
+				console.log("Tracking code is null or invalid");
+				return res.status(400).send();
+			}
+			const { data } = await limiter.schedule({ expiration: 1500 }, () => api.get('/', { params: { codigo: trackingCode } }));
+			const { codigo: code, ultimo: updatedAt, servico: type, eventos: events } = data;
+			return res.json({
+				code,
+				updatedAt,
+				type,
+				events: events.map(({ data: date, hora: time, local: location, status, subStatus }) => {
+					const [unity, city, state,] = location.split('/');
+					return {
+						date: DateTime.fromFormat(`${date} ${time}`, 'dd/MM/yyyy HH:mm').toISO(),
+						unity,
+						city,
+						state,
+						status,
+						subStatus
+					}
+				})
+			});
+		} catch (err) {
+			if (err instanceof Bottleneck.BottleneckError) {
+				return res.status(503).send();
+			}
+			console.error(err)
+			return res.status(500).json({ status: 500 });
+		}
+	}
 }
